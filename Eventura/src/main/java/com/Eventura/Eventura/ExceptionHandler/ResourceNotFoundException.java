467af405540d1c1655a0f7314fcb730767a3f5d2
Sunny-Hasho <sandeep.hasarinda@gmail.com>
2025-05-24 02:00:52 +0530

    merged eventura web

--- Eventura/src/main/java/com/Eventura/Eventura/ExceptionHandler/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.Eventura.Eventura.ExceptionHandler;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
