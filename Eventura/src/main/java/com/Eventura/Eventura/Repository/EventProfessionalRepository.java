2fbb1b3c824b3b92f8940a451abd03f13569f087
Sunny-Hasho <sandeep.hasarinda@gmail.com>
2025-05-24 02:00:52 +0530

    merged eventura web

--- Eventura/src/main/java/com/Eventura/Eventura/Repository/EventProfessionalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Eventura.Eventura.Repository;
-
-import com.Eventura.Eventura.Model.EventProfessional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventProfessionalRepository extends JpaRepository<EventProfessional, Long> {
-}
