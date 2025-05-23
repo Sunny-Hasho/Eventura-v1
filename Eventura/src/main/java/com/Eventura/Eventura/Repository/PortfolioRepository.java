64fb4df9f9ad74c86a680c18e91376b599eb3e3b
Sunny-Hasho <sandeep.hasarinda@gmail.com>
2025-05-24 02:00:52 +0530

    merged eventura web

--- Eventura/src/main/java/com/Eventura/Eventura/Repository/PortfolioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Eventura.Eventura.Repository;
-
-
-import com.Eventura.Eventura.Model.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-}
--- Eventura/src/main/java/com/Eventura/Eventura/Repository/ServiceProviderDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Eventura.Eventura.Repository;
-
-import com.Eventura.Eventura.Model.ServiceProviderDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceProviderDetailsRepository extends JpaRepository<ServiceProviderDetails, Long> {
-}
